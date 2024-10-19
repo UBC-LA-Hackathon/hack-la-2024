7ab95d033290110332dcd4048491e4b1900dfff3
nikipdsci <124737571+nikipdsci@users.noreply.github.com>
2024-10-19 12:28:59 -0700

    read txt file

--- script.py
+++ script.py
@@ -1,5 +1,10 @@
-import requests
+import os
 
-url = "POST|/api/v1/users/:user_id/tokens"
+# text files
+arts = open("Hack-LA-24-ARTS-Discussion-1.txt", "r")
+banksy = open("Hack-LA-24-Banksy-Discussion-1.txt", "r")
+genetic = open("Hack-LA-24-Genetic-Discussion-1.txt", "r")
+music = open("Hack-LA-24-Music-Discussion-1.txt", "r")
 
-token = requests.post(url)
+
+for
