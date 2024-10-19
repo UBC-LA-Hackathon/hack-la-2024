d22511606b45e0c37d3834576515d758d7b4e603
Vivaan Bhandary <vivaanbhandary@gmail.com>
2024-10-19 15:44:51 -0700

    added discussion spectater and initializer

--- objection/tryObj.py
+++ objection/tryObj.py
@@ -10,8 +10,31 @@ driver_path = "./chromedriver.exe"  # Ensure you have the correct path to your C
 service = Service(driver_path)  # Create a Service object
 options = Options()
 
-def post_user(user, driver):
-    driver.get('https://objection.lol/courtroom/3792uz')
+def new_discussion_spectate(discussiontitle, driver):
+    driver.get('https://objection.lol/courtroom')
+    driver.implicitly_wait(10)
+
+    text_input = driver.find_element(by=By.XPATH,
+                                     value='/html/body/div/div[1]/div[2]/main/div/div/form/div[1]/div[1]/div/div/div/div/input')
+    text_input.send_keys("\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b")
+    text_input.send_keys(discussiontitle)
+
+    text_input = driver.find_element(by=By.XPATH,
+                                     value='/html/body/div/div[1]/div[2]/main/div/div/form/div[2]/div[2]/div/div/div/div[1]/input')
+
+    discussion_code = text_input.get_attribute('value')
+    print(discussion_code)
+
+    submit_button = driver.find_element(by=By.XPATH, value='/html/body/div/div[1]/div[2]/main/div/div/form/div[4]/div/button')
+    submit_button.click()
+
+    spectate_button = driver.find_element(by=By.XPATH,
+                                        value='/html/body/div/div[3]/div/div/form/div[3]/button[1]')
+    spectate_button.click()
+
+    return discussion_code
+def post_user(user, disc_code, driver):
+    driver.get('https://objection.lol/courtroom/' + disc_code)
     driver.implicitly_wait(10)
 
     # Find and interact with the text input (example selector, modify as needed)
@@ -47,6 +70,8 @@ def post_argument(argument, driver):
 
 
 # Open two instances of Chrome with different options if needed
+driver0 = webdriver.Chrome()
+
 driver1 = webdriver.Chrome()
 # driver2 = webdriver.Chrome(service=service, options=options)
 
@@ -55,10 +80,12 @@ user1 = "phoenix"
 user2 = "miles"
 argument1 = "Your first argument here"
 argument2 = "Your second argument here"
+discussiontitle = "Goku vs The Sun"
 
-post_user(user1, driver1)
+disc_code = new_discussion_spectate(discussiontitle, driver0)
+post_user(user1, disc_code, driver1)
 
-# post_argument(argument1, driver1)  # Argument 1 posted in browser 1
+post_argument(argument1, driver1)  # Argument 1 posted in browser 1
 # time.sleep(20)  # Adding delay for timing (adjust as needed)
 # post_argument(argument2, driver2)  # Argument 2 posted in browser 2
 
@@ -68,5 +95,7 @@ post_user(user1, driver1)
 time.sleep(300)  # Adjust the time to keep the browser open before closing
 
 # Close the browser windows when done
+driver0.quit()
+
 driver1.quit()
 # driver2.quit()
