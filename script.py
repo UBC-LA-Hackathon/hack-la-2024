704625267499646b3c00cefe5eddf06a9b60329c
nikipdsci <124737571+nikipdsci@users.noreply.github.com>
2024-10-19 12:46:48 -0700

    comments

--- script.py
+++ script.py
@@ -1,10 +1,4 @@
-import os
 
-# text files
-arts = open("Hack-LA-24-ARTS-Discussion-1.txt", "r")
-banksy = open("Hack-LA-24-Banksy-Discussion-1.txt", "r")
-genetic = open("Hack-LA-24-Genetic-Discussion-1.txt", "r")
-music = open("Hack-LA-24-Music-Discussion-1.txt", "r")
-
-
-for
+# open banksy.json
+# read
+# add info to Discussion object
